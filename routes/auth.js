0e63da72c2706049cf8e4490695fdf772b9953c1
Mohammed Aamir Mansoori <aamir.mansoori2911@gmail.com>
2024-01-06 12:24:14 +0530

    Added a login endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = "A$MERN$PROJECT";
 
-// Create a user using: POST "/api/auth/createuser". Mo login required.
+// Create a user using: POST "/api/auth/createuser". No login required.
 router.post(
   "/createuser",
   body("name", "Enter a Valid Name").isLength({ min: 3 }).notEmpty(),
@@ -38,17 +38,59 @@ router.post(
           },
         };
         const authToken = jwt.sign(data, JWT_SECRET);
-        
-        return res.json({authToken});
+
+        return res.json({ authToken });
         // return res.json(user);
       }
     } catch (error) {
       return res.status(500).json({
-        error: "An error occurred while saving the user",
+        error: "Internal Server Error",
         message: error.message,
       });
     }
     res.send({ errors: result.array() });
+  }
+);
+
+// Create a user using: POST "/api/auth/loginuser". No login required.
+router.post(
+  "/loginuser",
+  body("email", "Enter a Valid Email").isEmail(),
+  body("password", "Please enter a password").exists(),
+  async (req, res) => {
+    // Validating the inputs if it is email and the password exists
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    // Check whether the user with same email exists already
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please enter correct credentials" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please enter correct credentials" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      return res.json({ authToken });
+    } catch (error) {
+      return res.status(500).json({
+        error: "Internal Server Error",
+        message: error.message,
+      });
+    }
   }
 );
 
